472200ebdba47be8d64f8a6d14f0b6ee7e8681d7
rdentato <rdentato@gmail.com>
2024-06-20 22:20:36 +0200

    Added /DNDEBUG

--- src/boot.bat
+++ src/boot.bat
@@ -1,7 +1,7 @@
 :: Build script for Micrsoft C
 
-cl /std:c11 /O2 /I..\extlibs /c /Fe:val.obj ..\extlibs\val.c
-cl /std:c11 /O2 /I..\extlibs /Fe:tng.exe boot\tng_boot.c val.obj
+cl /std:c11 /DNDEBUG /O2 /I..\extlibs /c /Fe:val.obj ..\extlibs\val.c
+cl /std:c11 /DNDEBUG /O2 /I..\extlibs /Fe:tng.exe boot\tng_boot.c val.obj
 tng -o tng.c tng.md
-cl /std:c11 /O2 /I..\extlibs /Fe:tng.exe tng.c val.obj
+cl /std:c11 /DNDEBUG /O2 /I..\extlibs /Fe:tng.exe tng.c val.obj
 del *.obj
